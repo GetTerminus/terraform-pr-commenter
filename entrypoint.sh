01a5069848804a23a5e9c353b6b1d3d3d5d61001
Jonathan Ogilvie <jonathan.ogilvie@terminus.com>
2022-03-07 14:07:13 -0500

    Don't regex replace in the context of '+/-'

--- entrypoint.sh
+++ entrypoint.sh
@@ -82,9 +82,10 @@ else
 fi
 
 # change diff character, a red '-', into a high unicode character \U1f605 (literally 😅)
+# iff not preceded by a literal "/" as in "+/-".
 # this serves as an intermediate representation representing "diff removal line" as distinct from
 # a raw hyphen which could *also* indicate a yaml list entry.
-INPUT=$(echo "$RAW_INPUT" | sed "s/\x1b\[31m-\x1b\[0m/😅/g")
+INPUT=$(echo "$RAW_INPUT" | sed "s/(?<!\/)\x1b\[31m-\x1b\[0m/😅/g")
 
 # now remove all ANSI colors
 INPUT=$(echo "$INPUT" | sed 's/\x1b\[[0-9;]*m//g')
