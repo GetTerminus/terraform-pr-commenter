aebb6a0ca71fc908e7977b8ff79be01e9897740a
dreinhardt-terminus <david.reinhardt@terminus.com>
2023-06-21 13:52:44 -0400

    feat: update delimiter for plan success

--- handlers/plan_handler.sh
+++ handlers/plan_handler.sh
@@ -55,7 +55,7 @@ post_plan_comments () {
   delimiter_end_cmd=$(delimiter_end_cmd_builder "/Plan: ")
 
   #clean_input=$(echo "$INPUT" | perl -pe'$_="" unless /(An execution plan has been generated and is shown below.|Terraform used the selected providers to generate the following execution|No changes. Infrastructure is up-to-date.|No changes. Your infrastructure matches the configuration.)/ .. 1')
-  clean_input=$(echo "$INPUT" | perl -pe'$start_delimiter')
+  clean_input=$(echo "$INPUT" | perl -pe "${delimiter_start_cmd}")
   clean_input=$(echo "$clean_input" | sed -r "${delimiter_end_cmd}")
 
   post_diff_comments "plan" "Terraform \`plan\` Succeeded for Workspace: \`$WORKSPACE\`" "$clean_input"
